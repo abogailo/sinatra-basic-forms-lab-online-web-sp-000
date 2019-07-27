c7d16798c43786888d18daf15d7650ef128e2e53
Abagail Hudson <abagailanne@gmail.com>
2019-07-27 14:36:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/new' do 
+      erb :create_puppy
+  end
 
   post '/puppy' do
     puppy = Puppy.new(params[:name], params[:breed], params[:age])
